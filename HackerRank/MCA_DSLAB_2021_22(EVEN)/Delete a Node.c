3efffae4b54045c0fb9832e45267bea8da628be1
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-07-01 23:32:55 +0530

    Create Delete a Node.c

---
+++ HackerRank/MCA_DSLAB_2021_22(EVEN)/Delete a Node.c
@@ -0,0 +1,27 @@
+/*
+ * Complete the 'deleteNode' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+
+    if((position) == 0) {
+        return llist->next;
+    }
+    llist->next = deleteNode(llist->next, position-1);
+    return llist;
+}
