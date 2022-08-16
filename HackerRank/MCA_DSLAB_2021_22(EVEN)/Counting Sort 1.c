b223ce2c44b52f6d2052af76043349a6dc783b9b
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-08-16 19:54:11 +0530

    Create Counting Sort 1.c

---
+++ HackerRank/MCA_DSLAB_2021_22(EVEN)/Counting Sort 1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+
+    int n,i;
+    scanf("%d",&n);
+    int b[100],a;
+    for(i=0;i<100;i++)
+    {   
+         b[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {    scanf("%d",&a);
+         b[a]++;
+    }
+    for(i=0;i<100;i++)
+    {   
+        printf("%d ", b[i]);
+    }
+   
+    return 0;
+}
