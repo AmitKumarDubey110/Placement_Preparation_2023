4888b955ec460fd6ed1ab132170ee41f3b2a3c47
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-10-01 22:09:10 +0530

    Create 378. Kth Smallest Element in a Sorted Matrix.java

---
+++ Leetcode/378. Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int[] mat : matrix)
+            for(int m : mat)
+                pq.offer(m);
+        while(--k != 0)
+            pq.poll();
+        return pq.peek();
+    }
+}
